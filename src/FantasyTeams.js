8d1da7421dc9abceaf125c2b3dafd2e76139ec26
Carl <carlbrainegebe@gmail.com>
2021-08-07 23:00:39 -0500

    create dropdown for rendering fantasy teams

--- src/FantasyTeams.js
+++ src/FantasyTeams.js
@@ -7,6 +7,7 @@ import {v4 as uuid} from "uuid"
 function FantasyTeams() {
     
     const [fantasyTeams, setFantasyTeams] = useState([])
+    const [selectedTeam, setSelectedTeam] = useState("none selected")
 
     useEffect(() => {
         fetch("http://localhost:9393/fantasy_teams/")
@@ -17,8 +18,28 @@ function FantasyTeams() {
     function handleTeamCreate(newTeam) {
         setFantasyTeams([...fantasyTeams, newTeam])
     }
-
     console.log("ALL TEAMS: ", fantasyTeams)
+    
+    function optionDropdown() {
+        return fantasyTeams.map( (team) => {
+            return <option key={uuid()} value={team.id}>{team.name}</option>
+        } )
+    }
+
+    function handleTeamSelect(e) {
+        const teamObj = fantasyTeams.find( (team) => team.id == e.target.value )
+        setSelectedTeam(teamObj)
+    }
+
+    function renderSelectedTeam() {
+        if(selectedTeam == "none selected") {
+            return <p>no team selected</p>
+        }
+        else {
+            return <FantasyTeam team={selectedTeam}/>
+        }
+    }
+
 
     const renderTeams = fantasyTeams.map( team => <FantasyTeam key={uuid()} team={team}/> )
 
@@ -26,7 +47,12 @@ function FantasyTeams() {
         <div> 
               <CreateFantasyTeam onTeamCreate={handleTeamCreate}/> 
               <h2>Fantasy Teams</h2> 
-              {renderTeams}
+              <select onChange={handleTeamSelect}>
+                  <option>select team</option>
+                  {optionDropdown()}
+              </select>
+              {renderSelectedTeam()}
+              {/* {renderTeams} */}
         </div>
     )
 }
